6733a9eefd727e7152202be90178075f284b3857
Rafael Tirado <llanero7@gmail.com>
2017-04-29 00:29:34 +0000

    We did it reddit

--- highest_number_cubed/main.py
+++ highest_number_cubed/main.py
@@ -2,4 +2,19 @@
 
 
 def highest_number_cubed(limit):
-    pass
+    prev_num = 0
+    
+    while (prev_num - 1) ** 3 < limit: 
+        current_num = prev_num + 1
+        if current_num ** 3 > limit:
+            return prev_num
+            
+        prev_num = current_num
+
+
+
+# 1 ^ 3 = 1 < 30? Yes
+# 2 ^ 3 = 8 < 30? Yes
+# 3 ^ 3 = 27 < 30? Yes
+#         30
+# 4 ^ 3 = 64 < 30? NO 
